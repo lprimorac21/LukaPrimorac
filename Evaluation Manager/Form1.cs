a07cfec8c569ac3c4b1da9094df2032edc5fa901
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-04-24 20:48:58 +0200

    Prijava

--- Evaluation Manager/Form1.cs
+++ Evaluation Manager/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace Evaluation_Manager
 {
-    public partial class Form1 : Form
+    public partial class FrmLogin : Form
     {
-        public Form1()
+        string username = "nastavnik";
+        string passcode = "test";
+        public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text =="")
+            {
+                MessageBox.Show("Lozinka nije unesena", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            {
+                if (txtUsername.Text == username && txtPassword.Text == passcode) ;
+                {
+                    MessageBox.Show("Dobrodošli", "Prijavljeni ste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Krivi podaci", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
